55d02a2496ad8550cd13e2550518ea31eae8a3d7
KelseyRussell <kelseyrussell2002@gmail.com>
2024-06-30 11:29:55 -0600

    push

--- wa11/wa11.js
+++ wa11/wa11.js
@@ -5,21 +5,19 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-
-const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+const images = ['Image1.jpg', 'Image2.jpg', 'Image3.jpg', 'Image4.jpg', 'Image5.jpg'];
 const alts = {
-  'pic1.jpg' : 'Picture of an eye',
-  'pic2.jpg' : 'Picture of rocks',
-  'pic3.jpg' : 'Picture of flowers',
-  'pic4.jpg' : 'Picture of an Egyptian painting',
-  'pic5.jpg' : 'Picture of a butterfly'
-}
+  'Image1.jpg': 'Picture of an owl',
+  'Image2.jpg': 'Picture of a cat',
+  'Image3.jpg': 'Picture of friends',
+  'Image4.jpg': 'Picture of a sunset',
+  'Image5.jpg': 'Picture of a family'
+};
 
 /* Looping through images */
-
 for (const image of images) {
   const newImage = document.createElement('img');
-  newImage.setAttribute('src', `images/${image}`);
+  newImage.setAttribute('src', image); 
   newImage.setAttribute('alt', alts[image]);
   thumbBar.appendChild(newImage);
   newImage.addEventListener('click', e => {
@@ -29,15 +27,14 @@ for (const image of images) {
 }
 
 /* Wiring up the Darken/Lighten button */
-
 btn.addEventListener('click', () => {
   const btnClass = btn.getAttribute('class');
   if (btnClass === 'dark') {
-    btn.setAttribute('class','light');
+    btn.setAttribute('class', 'light');
     btn.textContent = 'Lighten';
     overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
   } else {
-    btn.setAttribute('class','dark');
+    btn.setAttribute('class', 'dark');
     btn.textContent = 'Darken';
     overlay.style.backgroundColor = 'rgba(0,0,0,0)';
   }
