05f7f0fba72cf3a35e741d600123a865c4e6875e
KelseyRussell <kelseyrussell2002@gmail.com>
2024-06-30 18:59:15 -0600

    push

---
+++ final/final.js
@@ -0,0 +1,74 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var numberBox = document.getElementById('numberBox');
+    var responseSection = document.getElementById('responseSection');
+    var submitButton = document.getElementById('submitButton');
+    var container = document.querySelector('.container');
+  
+    function generateRandomNumber() {
+      return Math.floor(Math.random() * (9999999999 - 1000000000 + 1)) + 1000000000;
+    }
+  
+    function showResponseSection() {
+      responseSection.style.display = 'block';
+    }
+  
+    function createNewResponseSection() {
+      var newResponseSection = document.createElement('div');
+      newResponseSection.classList.add('response-section');
+  
+      var newNumberBox = document.createElement('div');
+      newNumberBox.classList.add('number-box');
+      var newRandomNumber = generateRandomNumber(); 
+      newNumberBox.innerText = newRandomNumber; 
+  
+      var newInputLabel = document.createElement('label');
+      newInputLabel.setAttribute('for', 'response');
+      newInputLabel.textContent = 'Is this your number? (Type "yes" or "no")';
+      var newInput = document.createElement('input');
+      newInput.setAttribute('type', 'text');
+      newInput.setAttribute('class', 'response'); 
+  
+      var newButton = document.createElement('button');
+      newButton.textContent = 'Submit';
+      newButton.addEventListener('click', function() {
+        checkNumber(newInput, newNumberBox);
+      });
+  
+      newResponseSection.appendChild(newNumberBox);
+      newResponseSection.appendChild(newInputLabel);
+      newResponseSection.appendChild(newInput);
+      newResponseSection.appendChild(newButton);
+  
+      container.appendChild(newResponseSection);
+    }
+  
+    function checkNumber(inputField, numberBox) {
+      var randomNumber = generateRandomNumber();
+      numberBox.innerText = randomNumber;
+  
+      var response = inputField.value.toLowerCase();
+  
+      if (response === 'yes') {
+        alert('Congratulations! Your number is ' + randomNumber);
+        responseSection.style.display = 'none'; 
+      } else if (response === 'no') {
+        createNewResponseSection(); 
+        inputField.value = ''; 
+      } else {
+        alert('Please type "yes" or "no".');
+      }
+    }
+  
+    var initialRandomNumber = generateRandomNumber();
+    numberBox.innerText = initialRandomNumber;
+    showResponseSection();
+  
+    submitButton.addEventListener('click', function() {
+      var responseInput = document.getElementById('response');
+      checkNumber(responseInput, numberBox);
+    });
+  });
+
+
+  
+  
