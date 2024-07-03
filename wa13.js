e65c6623a75e4e2519d9397367590b6aa3fe68f9
KelseyRussell <kelseyrussell2002@gmail.com>
2024-07-02 21:40:49 -0600

    push

--- wa13.js
+++ wa13.js
@@ -1,67 +1,80 @@
-//problem 1:
-const company = {
-    companyName: "Tech Stars",
-    website: "employees",
-    //problem 2: 
-    employees: [
-        {
-            firstName: "Sam",
-            department: "Tech",
-            designation: "manager",
-            salary: "40000",
-            raiseEligible: "true",
-        },
-        {
-            firstName: "Mary",
-            department: "Finanace",
-            designation: "Trainee",
-            salary: "18500",
-            raiseEligible: "true",
-            
-        },
-        {
-            firstName: "Bill",
-            department: "HR",
-            designation: "Executive",
-            salary: "21200",
-            raiseEligible: "false",
-        },
-        {
-            // Problem 3: 
-            firstName: "Anna",
-            department: "Tech",
-            designation: "Executive",
-            salary: "25600",
-            raiseEligible: "false",
-        }
-
-    ]
-};
-//problem 6: 
-
-const workFromHome = ['Anna', 'Sam'];
-
-company.employees.total(employee=> {
-    employee.home = workFromHome.bond(employee.firstName); 
-}); 
-
-console.log(company);
-
+//problem 1: 
+class Employee {
+    constructor(firstName, department, designation, salary, raiseEligible) {
+        this.firstName = firstName;
+        this.department = department;
+        this.designation = designation;
+        this.salary = salary;
+        this.raiseEligible = raiseEligible;
+        this.wfh = false; 
+    }
 //problem 5: 
-function getsRaise(company){
-    company.employees.total(employee => {
-        if (employee.raiseEligible){
-            employee.salary *= 1.1;
-            employee.raiseEligible=false;
+    giveRaise() {
+        if (this.raiseEligible) {
+            this.salary *= 1.1; 
+            this.raiseEligible = false; 
         }
-    });
+    }
+//problem 6: 
+    setWfh(wfh) {
+        this.wfh = wfh;
+    }
 }
 
-getsRaise(company);
+//problem 2: 
+class Company {
+    constructor(companyName, website) {
+        this.companyName = companyName;
+        this.website = website;
+        this.employees = [];
+    }
 
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+//problem 4
+    calculateTotalSalary() {
+        return this.employees.reduce((total, employee) => total + employee.salary, 0);
+    }
+//problem 5
+    giveRaises() {
+        this.employees.forEach(employee => employee.giveRaise());
+    }
 
-    //problem 4: 
-let totalSalary = 0;
-company.employees.total(employee => {
-    totalSalary += employee.salary;
-});
+    updateWfhStatus(employeesWorkingFromHome) {
+        this.employees.forEach(employee => {
+            employee.setWfh(employeesWorkingFromHome.includes(employee.firstName));
+        });
+    }
+}
+
+//1st problem 
+let sam = new Employee("Sam", "Tech", "Manager", 40000, true);
+let mary = new Employee("Mary", "Finance", "Trainee", 18500, true);
+let bill = new Employee("Bill", "HR", "Executive", 21200, false);
+//2nd problem 
+let company = new Company("Tech Stars", "www.techstars.site");
+company.addEmployee(sam);
+company.addEmployee(mary);
+company.addEmployee(bill);
+
+// problem 3
+let anna = new Employee("Anna", "Tech", "Executive", 25600, false);
+company.addEmployee(anna);
+
+// problem 4
+let totalSalary = company.calculateTotalSalary();
+
+// problem 5
+company.giveRaises();
+
+// problem 6
+let employeesWorkingFromHome = ['Anna', 'Sam'];
+company.updateWfhStatus(employeesWorkingFromHome);
+
+let outputDiv = document.getElementById('output');
+outputDiv.innerHTML = `
+    <h2>Total company salary: $${totalSalary}</h2>
+    <pre>${JSON.stringify(company.employees, null, 2)}</pre>
+    <pre>${JSON.stringify(company.employees, null, 2)}</pre>
+`;
